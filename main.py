2df475cbeffbd46be8f68f15d7edc3b640166d22
juxhinb7 <juxhinbazelli@gmail.com>
2025-01-12 13:22:28 +0100

    added cors util

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ from llama_index.llms.openai import OpenAI
 import os
 
 from flask import Flask, stream_with_context
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/streaming")
 def streaming():
