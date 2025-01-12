657ba7407aeb55442879f041933cdcec54c1df25
juxhinb7 <juxhinbazelli@gmail.com>
2025-01-12 17:55:19 +0100

    post request added

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ from llama_index.core import (
 from llama_index.llms.openai import OpenAI
 import os
 
-from flask import Flask, stream_with_context
-from flask_cors import CORS, cross_origin
+from flask import Flask, stream_with_context, request
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/streaming")
+@app.route("/streaming", methods=["POST"])
 def streaming():
+    content = request.get_json()
 
     # check if storage already exists
     PERSIST_DIR = "./storage"
@@ -35,5 +36,5 @@ def streaming():
     # Either way we can now query the index
     Settings.llm = OpenAI(model="gpt-4o-mini")
 
-    return stream_with_context(index.as_query_engine(streaming=True).query("How can i write the fibonacci function in php? come up with the code").response_gen)
+    return stream_with_context(index.as_query_engine(streaming=True).query(content["prompt"]).response_gen)
 
